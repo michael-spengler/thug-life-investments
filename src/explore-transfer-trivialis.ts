44bba2149aa321de0cca4942b9c4e59cd8c20672
Michael <michael.spengler@sap.com>
2021-01-02 02:26:49 +0100

    showcase a simple transfer of ether from one wallet to another

---
+++ src/explore-transfer-trivialis.ts
@@ -0,0 +1,42 @@
+
+require('dotenv').config()
+
+const Web3 = require('web3');
+const Tx = require('ethereumjs-tx').Transaction;
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`));
+
+export const transfer = async () => {
+
+    web3.eth.getGasPrice().then((gasPrice) => {
+        web3.eth.getTransactionCount(process.env.SOURCE_ACCOUNT, 'pending').then((nonce) => {
+            let txParams = {
+                nonce: web3.utils.toHex(nonce),
+                gasLimit: web3.utils.toHex(web3.utils.toHex(33000)),
+                gasPrice: web3.utils.toHex(gasPrice),
+                from: process.env.SOURCE_ACCOUNT,
+                to: process.env.ACCOUNT,
+                value: 0.07,
+                chainId: web3.utils.toHex(1),
+            }
+            let tx = new Tx(txParams);
+            let privKey = Buffer.from(process.env.SOURCE_ACCOUNT_PRIVATE_KEY, 'hex');
+            tx.sign(privKey);
+            tx = "0x" + tx.serialize().toString('hex')
+            web3.eth.sendSignedTransaction(tx)
+                .on('transactionHash', (hash) => {
+                    console.log(hash);
+                })
+                .on('receipt', (receipt) => {
+                    console.log(receipt);
+                    return "receipt"
+                })
+                .on('confirmation', (confirmationNumber, receipt) => {
+                    console.log(confirmationNumber);
+                    console.log(receipt);
+                })
+                .on('error', console.error);
+        })
+    });
+}
+
+transfer()
